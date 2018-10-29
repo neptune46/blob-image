1d3a6f8687edeb0b454a315f66da13e5d07b474c
neptune46 <neptune46@outlook.com>
2018-10-29 22:05:16 +0800

    define swblob interface

--- src/lib/swblob.cpp
+++ src/lib/swblob.cpp
@@ -1,6 +1,6 @@
 #include "swblob.h"
 
-int blobFromImage(char* src, char*dst, double scale, Size2D sz, MeanScale ms, bool swapRB, bool crop)
+int blobFromImage(BlobImage<char>* src, BlobImage<float>* dst, double scale, Size2D sz, MeanScale ms, bool swapRB, bool crop)
 {
     return 0;
 }
--- src/lib/swblob.h
+++ src/lib/swblob.h
@@ -1,11 +1,25 @@
 
-typedef struct _Size2D{
+struct Size2D{
     int w;
     int h;
-} Size2D;
+};
 
-typedef struct _MeanScale{
+struct MeanScale{
     double scale[4];
-} MeanScale;
+};
 
-int blobFromImage(char* src, char*dst, double scale, Size2D sz, MeanScale ms, bool swapRB, bool crop);
+enum ImageFmt {
+    IMG_FMT_YUYV,
+    IMG_FMT_RGB24
+};
+
+template <typename T>
+struct BlobImage {
+    int w;
+    int h;
+    ImageFmt fmt;
+    int size[4];
+    T* data[4];
+};
+
+int blobFromImage(BlobImage<char>* src, BlobImage<float>* dst, double scale, Size2D sz, MeanScale ms, bool swapRB, bool crop);
