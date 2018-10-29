ded05b7be13d2b006e38e193b7ae443dc70f6ece
neptune46 <neptune46@outlook.com>
2018-10-29 22:59:38 +0800

    test interface of libswblob

--- src/test/test_swblob.cpp
+++ src/test/test_swblob.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
+    BlobImage<char> src;
+    BlobImage<float> dst;
+    double scale = 1.0;
+    Size2D sz = {300, 300};
+    MeanScale ms = {0.0, 0.0, 0.0, 0.0};
+
+    blobFromImage(&src, &dst, scale, sz, ms, false, false);
+
     return 0;
 }
 
